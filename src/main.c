841e109c6a864f15a1f234ccea989fa9dad44a18
Tomás Andrade <tomas.andrade2043@gmail.com>
2025-11-11 13:13:32 +0000

    Finished, missing updated textures

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: toandrad <toandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 13:50:19 by tomas             #+#    #+#             */
-/*   Updated: 2025/11/11 11:24:00 by toandrad         ###   ########.fr       */
+/*   Updated: 2025/11/11 12:31:24 by toandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int ac, char **av)
 		return(ft_putstr_fd("Usage: ./so_long <map.ber>\n", 2), 1);
 	game = init_game(av[1]);
 	render_map(game);
-	mlx_hook(game->win, 2, 1L << 0, handle_close, game);
+	mlx_hook(game->win, 2, 1L << 0, handle_keypress, game);
 	mlx_hook(game->win, 17, 0, handle_close, game);
 	mlx_loop(game->mlx);
 	return (0);
